2451f2ce8fd2ea2b65b063c68a526f2281221d15
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2019-05-17 16:53:01 +0200

    conda_install.sh: fix miniconda3 install

--- conda_install.sh
+++ conda_install.sh
@@ -1,4 +1,5 @@
-wget -qO- https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh | bash <<EOF
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh && \
+bash Miniconda3-latest-Linux-x86_64.sh <<EOF
 
 yes
 
